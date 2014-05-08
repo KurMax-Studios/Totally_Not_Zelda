63102210e553e9c65c0d344170f7086278c359bf
Emil Jinstrand <emil.jinstrand@gmail.com>
2014-05-08 18:18:20 +0200

    More work on EnemyManager

--- Totally Not Zelda/EnemyManager.cpp
+++ Totally Not Zelda/EnemyManager.cpp
@@ -9,3 +9,14 @@ EnemyManager::EnemyManager(void)
 EnemyManager::~EnemyManager(void)
 {
 }
+
+void EnemyManager::update()
+{
+	for(size_t i=0; i < m_enemies.size(); i++)
+	{
+		if(m_enemies[i] != 0x0)
+		{
+			m_enemies[i]->update();
+		}
+	}
+}
